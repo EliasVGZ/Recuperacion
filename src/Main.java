eb54b13fd05705153ff9ac88f75c63f8c5c9ebca
EliasVGZ <elias.bs2@hotmail.com>
2023-01-03 16:42:56 +0100

    añadiendo una rama para probar

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
---
+++ src/NumeroAzar.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+public class NumeroAzar {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        Random r = new Random();
+        int azar = r.nextInt(100)+1;
+        System.out.println(azar);
+        int num=0;
+        int oportunidades=5;
+
+        do{
+            System.out.println("ADIVINA NUMERO ENTRE 0 Y 100, TIENES 5 OPORTUNIDADES");
+            num = Integer.parseInt(br.readLine());
+            oportunidades--;
+            if (num==azar) {
+                System.out.println("LO HAZ ACERTADO");
+            }
+            else if(num>azar){
+                System.out.println("El numero es mayor");
+                System.out.println("Te quedan " + oportunidades+ " oportunidades");
+            }else{
+                System.out.println("El numero es menor");
+                System.out.println("Te quedan " + oportunidades+ " oportunidades");
+            }
+            if(oportunidades==0){
+                System.out.println("No te quedan oportunidades, el numero era "+azar);
+            }
+
+        }while(azar!=num && oportunidades>0);
+    }
+}
